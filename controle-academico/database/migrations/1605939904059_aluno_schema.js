22852c189c9adc9a9b113b374f077ca8b56c5ba1
renansycer <renansycer@outlook.com>
2020-11-21 03:29:17 -0300

    Criando controller e models do Aluno

---
+++ controle-academico/database/migrations/1605939904059_aluno_schema.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class AlunoSchema extends Schema {
+  up () {
+    this.create('alunos', (table) => {
+      table.increments()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('alunos')
+  }
+}
+
+module.exports = AlunoSchema
